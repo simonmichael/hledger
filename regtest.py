a1af47976a8d609b795decc8e35abc2303678f66
Simon Michael <simon@joyful.com>
2007-08-04 14:32:57 +0000

    add regtest.py

---
+++ regtest.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# test whether hledger output matches ledger's
+
+from os import *
+from posix import *
+
+files = [
+    'test.dat',
+    'test1.dat',
+#    getenv('LEDGER'),
+    ]
+
+commands = [
+    'balance petty',
+    '-s balance',
+    '-s balance cash',
+    'register',
+    'register cash',
+    'print',
+    ]
+
+do = system
+rule = lambda s: "="*30 + s + "="*30
+
+def regtest(file, cmd):
+    """Print a heading and the diff of ledger and hledger output. No diff
+    output is good."""
+    print rule('%s:%s' % (file,cmd))
+    putenv('LEDGER',file)
+    do('ledger %s >1; ./hledger.hs %s >2; diff 1 2' % (cmd,cmd))
+
+for f in files:
+    for c in commands:
+        regtest(f,c)
+
