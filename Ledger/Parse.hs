4ea30b7411cf03e785a66ec7c7ffc04de69dace4
nick <nick@ingolia.org>
2008-12-08 07:20:29 +0000

    Fixed ;-only bug

--- Ledger/Parse.hs
+++ Ledger/Parse.hs
@@ -230,7 +230,7 @@ See "Tests" for sample data.
 
 emptyLine :: GenParser Char st ()
 emptyLine = do many spacenonewline
-               optional $ char ';' >> spacenonewline >> many (noneOf "\n")
+               optional $ char ';' >> many (noneOf "\n")
                newline
                return ()
 
