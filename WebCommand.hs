7d80cffb45df4ef1b7acdf8f207523ffbd4ce68b
Simon Michael <simon@joyful.com>
2009-01-20 04:28:42 +0000

    happs command cleanup

--- WebCommand.hs
+++ WebCommand.hs
@@ -28,18 +28,6 @@ import RegisterCommand
 import PrintCommand
 
 
--- | The application state when running the ui command.
-data AppState = AppState {
-     aw :: Int                   -- ^ window width
-    ,ah :: Int                   -- ^ window height
-    ,amsg :: String              -- ^ status message
-    ,aopts :: [Opt]              -- ^ command-line opts
-    ,aargs :: [String]           -- ^ command-line args
-    ,aledger :: Ledger           -- ^ parsed ledger
-    ,abuf :: [String]            -- ^ lines of the current buffered view
-                                -- ^ never null, head is current location
-    } deriving (Show)
-
 tcpport = 5000
 
 web :: [Opt] -> [String] -> Ledger -> IO ()
