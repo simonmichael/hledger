b27c90aea54395dd31583f5cdcc3f32117d6e84c
Simon Michael <simon@joyful.com>
2011-06-13 20:46:29 +0000

    fix "import Hledger", and also include Hledger.Utils, for easier scripting

---
+++ hledger-lib/Hledger.hs
@@ -0,0 +1,9 @@
+module Hledger (
+                module Hledger.Data
+               ,module Hledger.Read
+               ,module Hledger.Utils
+)
+where
+import Hledger.Data
+import Hledger.Read
+import Hledger.Utils
