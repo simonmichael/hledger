d4a3a8c3a0ba8b422f4ca1b1e49eea3ad5b404ae
Simon Michael <simon@joyful.com>
2011-09-23 14:07:50 +0000

    web: do auto-create the journal for the web command At add time would be better, but for at startup is easier

--- hledger-web/hledger-web.hs
+++ hledger-web/hledger-web.hs
@@ -43,7 +43,7 @@ runWith opts = run opts
           | "help" `in_` (rawopts_ $ cliopts_ opts)            = putStr (showModeHelp webmode) >> exitSuccess
           | "version" `in_` (rawopts_ $ cliopts_ opts)         = putStrLn progversion >> exitSuccess
           | "binary-filename" `in_` (rawopts_ $ cliopts_ opts) = putStrLn (binaryfilename progname)
-          | otherwise                                          = withJournalDo' opts web
+          | otherwise                                          = journalFilePathFromOpts (cliopts_ opts) >>= ensureJournalFile >> withJournalDo' opts web
 
 withJournalDo' :: WebOpts -> (WebOpts -> Journal -> IO ()) -> IO ()
 withJournalDo' opts cmd = do
