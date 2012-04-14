abb470aadbb268a834972b2cddb4a8f0b7bdb6ad
Simon Michael <simon@joyful.com>
2012-04-14 20:35:51 +0000

    use shakespeare-text to help lay out incomestatement's output

--- hledger/Hledger/Cli/Incomestatement.hs
+++ hledger/Hledger/Cli/Incomestatement.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE QuasiQuotes, TemplateHaskell, OverloadedStrings #-}
 {-|
 
 The @incomestatement@ command prints a fairly standard income statement (profit & loss) report.
@@ -9,24 +10,23 @@ module Hledger.Cli.Incomestatement (
  ,tests_Hledger_Cli_Incomestatement
 ) where
 
-import Data.List
+import qualified Data.Text.Lazy.IO as LT
 import Test.HUnit
+import Text.Shakespeare.Text
 
 import Hledger
-import Prelude hiding (putStr)
-import Hledger.Utils.UTF8IOCompat (putStr)
 import Hledger.Cli.Options
 import Hledger.Cli.Balance
-
 
 -- | Print a standard income statement.
 incomestatement :: CliOpts -> Journal -> IO ()
 incomestatement CliOpts{reportopts_=ropts} j = do
-  let lines = case formatFromOpts ropts of
-            Left err -> [err]
-            Right _ -> accountsReportAsText ropts $ accountsReport2 ropts (journalProfitAndLossAccountQuery j) j
-  putStr $ unlines lines
+  let report = accountsReport2 ropts (journalProfitAndLossAccountQuery j) j
+  LT.putStr $ [lt|Income Statement
 
+#{unlines $ accountsReportAsText ropts report}|]
+
+tests_Hledger_Cli_Incomestatement :: Test
 tests_Hledger_Cli_Incomestatement = TestList
  [
  ]
