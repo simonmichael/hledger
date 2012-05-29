5b749ab8a25522986bffc390b695af69af6bcfd3
Simon Michael <simon@joyful.com>
2012-05-29 16:45:24 +0000

    web: work around static file problem by skipping etag parameter

--- hledger-web/Hledger/Web/Settings/StaticFiles.hs
+++ hledger-web/Hledger/Web/Settings/StaticFiles.hs
@@ -35,4 +35,4 @@ staticSite = do
 --   giving you compile-time verification that referenced files exist.
 --   Warning: any files added to your static directory during run-time can't be
 --   accessed this way. You'll have to use their FilePath or URL to access them.
-$(staticFiles staticDir)
+$(publicFiles staticDir)
