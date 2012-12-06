7d1fce42fed78a1e6968138debddfc3595a365c7
Simon Michael <simon@joyful.com>
2012-12-06 03:56:12 +0000

    refactor: dead code

--- hledger-lib/Hledger/Data/Transaction.hs
+++ hledger-lib/Hledger/Data/Transaction.hs
@@ -24,7 +24,6 @@ module Hledger.Data.Transaction (
   -- * date operations
   transactionActualDate,
   transactionEffectiveDate,
-  transactionWithDate,
   -- * arithmetic
   transactionPostingBalances,
   balanceTransaction,
@@ -349,17 +348,6 @@ transactionActualDate = tdate
 -- Get a transaction's effective date, defaulting to the actual date.
 transactionEffectiveDate :: Transaction -> Day
 transactionEffectiveDate t = fromMaybe (tdate t) $ teffectivedate t
-
--- | Once we no longer need both, set the main transaction date to either
--- the actual or effective date. A bit hacky.
-transactionWithDate :: WhichDate -> Transaction -> Transaction
-transactionWithDate ActualDate t = t
-transactionWithDate EffectiveDate t =
-  txnTieKnot t{tdate=transactionEffectiveDate t, tpostings=map (postingWithDate EffectiveDate) $ tpostings t}
-
-postingWithDate :: WhichDate -> Posting -> Posting
-postingWithDate ActualDate p = p
-postingWithDate EffectiveDate p = p{pdate=pdate2 p}
 
 -- | Ensure a transaction's postings refer back to it.
 txnTieKnot :: Transaction -> Transaction
