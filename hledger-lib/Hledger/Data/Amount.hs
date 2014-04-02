21575ed47b75392838306999dec7c7820678fd39
Simon Michael <simon@joyful.com>
2014-04-02 04:57:33 -0700

    more verbose Amount debug output at level 6+

--- hledger-lib/Hledger/Data/Amount.hs
+++ hledger-lib/Hledger/Data/Amount.hs
@@ -117,9 +117,9 @@ amountstyle = AmountStyle L False 0 '.' ',' []
 
 instance Show Amount where
   show _a@Amount{..}
-    --  debugLevel < 3 = showAmountWithoutPrice a
-    --  debugLevel < 6 = showAmount a
-    | debugLevel < 9 =
+    --  debugLevel < 2 = showAmountWithoutPrice a
+    --  debugLevel < 3 = showAmount a
+    | debugLevel < 6 =
        printf "Amount {acommodity=%s, aquantity=%s, ..}" (show acommodity) (show aquantity)
     | otherwise      = --showAmountDebug a
        printf "Amount {acommodity=%s, aquantity=%s, aprice=%s, astyle=%s}" (show acommodity) (show aquantity) (showPriceDebug aprice) (show astyle)
