eea10d4420f8c93c6fb75bff8cc08392763855c5
Simon Michael <simon@joyful.com>
2014-05-23 09:34:32 -0700

    web: upgrade jquery to 2.1.1

--- hledger-web/Foundation.hs
+++ hledger-web/Foundation.hs
@@ -105,7 +105,7 @@ instance Yesod App where
             $(widgetFile "normalize")
             addStylesheet $ StaticR css_bootstrap_min_css
             -- load jquery early:
-            toWidgetHead [hamlet| <script type="text/javascript" src="@{StaticR jquery_min_js}"></script> |]
+            toWidgetHead [hamlet| <script type="text/javascript" src="@{StaticR js_jquery_min_js}"></script> |]
             addScript $ StaticR jquery_url_js
             addScript $ StaticR jquery_flot_min_js
             toWidget [hamlet| \<!--[if lte IE 8]> <script type="text/javascript" src="@{StaticR excanvas_min_js}"></script> <![endif]--> |]
