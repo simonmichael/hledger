67c8ceb142028a3ca8c2e0e89c13cabb93daaae2
Simon Michael <simon@joyful.com>
2014-07-12 15:18:39 -0700

    web: after adding, just redisplay the journal

--- hledger-web/Handler/Post.hs
+++ hledger-web/Handler/Post.hs
@@ -90,10 +90,9 @@ handleAdd = do
     let t' = txnTieKnot t -- XXX move into balanceTransaction
     liftIO $ do ensureJournalFileExists journalpath
                 appendToJournalFileOrStdout journalpath $ showTransaction t'
-    -- setMessage $ toHtml $ (printf "Added transaction:\n%s" (show t') :: String)
-    setMessage [shamlet|<span>Added transaction:<small><pre>#{chomp $ show t'}</pre></small>|]
+    setMessage [shamlet|<span>Transaction added.|]
 
-  redirect (JournalR, [("add","1")])
+  redirect (JournalR) -- , [("add","1")])
 
 -- | Handle a post from the journal edit form.
 handleEdit :: Handler Html
