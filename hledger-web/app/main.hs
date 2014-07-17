927e7593fa633f580722a42b164fa095bbff0a1e
Simon Michael <simon@joyful.com>
2014-07-17 15:05:39 -0700

    web: cleanups

--- hledger-web/app/main.hs
+++ hledger-web/app/main.hs
@@ -1,11 +1,20 @@
 import Prelude              (IO)
+
+import qualified Hledger.Web.Main
+
+main :: IO ()
+main = Hledger.Web.Main.main
+
+-- more standard yesod main, for reloading experiments
+
 -- import Yesod.Default.Config (fromArgs)
 -- import Yesod.Default.Main   (defaultMain)
 -- import Settings             (parseExtra)
 -- import Application          (makeApplication)
 
-import qualified Hledger.Web.Main
+-- import Hledger
+-- import Hledger.Web (defwebopts)
 
-main :: IO ()
--- main = defaultMain (fromArgs parseExtra) makeApplication
-main = Hledger.Web.Main.main
+-- main = do
+--   j <- defaultJournal
+--   defaultMain (fromArgs parseExtra) (makeApplication defwebopts j)
