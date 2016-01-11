88d8f975ede1027f9b19de914aa54178b4d83a1a
Thomas R. Koll <tomk32@tomk32.de>
2016-01-11 23:16:00 +0100

    Start client app using AngularJS #316

---
+++ hledger-api/client/hledger-client.js
@@ -0,0 +1,39 @@
+var hledger = angular.module('hledger', [
+  'ui.router',
+  'ngResource'
+])
+hledger.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/accounts");
+  $stateProvider
+    .state('accounts', {
+      url: "/accounts",
+      templateUrl: "accounts/view.html",
+      controller: function($scope) {
+        $scope.accounts = ["A", "List", "Of", "Items"];
+      }
+    })
+    .state('help', {
+      url: "/help",
+      templateUrl: "help/index.html"
+    });
+});
+
+hledger.factory('Journal', function($resource) {
+  return($resource("/journals/:id"));
+});
+
+hledger.controller("JournalController", function($scope, Journal) {
+  Journal.query(function(data) {
+   $scope.journals = data;
+  });
+});
+
+hledger.factory('Account', function($resource) {
+  return($resource("/accounts/:id"));
+});
+
+hledger.controller("AccountsController", function($scope, Account) {
+  Account.query(function(data) {
+   $scope.accounts = data;
+  });
+});
