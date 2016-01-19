ee2c06c3202f8cadaae1455cb228b5bae5965a47
Thomas R. Koll <tomk32@tomk32.de>
2016-01-19 10:13:01 +0100

    api-client: Use accounts.asub instead of children, needs fixing of the api

--- hledger-api/static/hledger-client.js
+++ hledger-api/static/hledger-client.js
@@ -3,19 +3,6 @@ var hledger = angular.module('hledger', [
   'ngResource'
 ])
 hledger.api_root = '/api/v1';
-function listToTree(list, id_field, parent_field) {
-  children = function(list, parent_id) {
-    return $.grep(list,
-      function(element) {
-        return element[parent_field] === parent_id
-      });
-  }
-  $.map(list, function(element) {
-    element.children = children(list, element[id_field])
-  });
-  root = children(list, '');
-  return root;
-}
 
 hledger.config(function($stateProvider, $urlRouterProvider) {
   //$urlRouterProvider.otherwise("/");
@@ -60,7 +47,7 @@ hledger.factory('Account', function($resource) {
 
 hledger.controller("AccountsController", function($scope, Account) {
   Account.query(function(data) {
-    $scope.accounts = listToTree(data, 'aname', 'aparentname')[0].children;
+    $scope.accounts = data;
   });
 });
 
