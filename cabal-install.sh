5aa4c9133232134f0f426ef3defd7374504c92db
Simon Michael <simon@joyful.com>
2016-10-21 14:47:22 -0700

    stack/cabal cleanups; drop GHC 7.6, 7.8 support
    A (disabled) stack build plan for GHC 7.8 is included; supporting it
    would need small code changes and a maintainer.

--- cabal-install.sh
+++ cabal-install.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
-# Run this script to install all hledger packages using cabal
-# (if you prefer using cabal to stack)
+# Install all hledger packages using cabal (if you prefer it to stack).
+# cabal.project may be a newer way.
 
-# make a sandbox in this directory to avoid build problems
+# Make a local sandbox - optional, recommended for reliable installs
 cabal sandbox init
 
-# might be useful for some developers..
-cabal sandbox add-source ./hledger-lib
-cabal sandbox add-source ./hledger
-cabal sandbox add-source ./hledger-ui
-cabal sandbox add-source ./hledger-web
-cabal sandbox add-source ./hledger-api
+# Tell sandbox about the local packages, recommended for developers
+#cabal sandbox add-source ./hledger-lib
+#cabal sandbox add-source ./hledger
+#cabal sandbox add-source ./hledger-ui
+#cabal sandbox add-source ./hledger-web
+#cabal sandbox add-source ./hledger-api
 
-# build and install
-cabal install ./hledger-lib ./hledger ./hledger-ui ./hledger-web ./hledger-api \
-  && echo "hledger executables successfully installed in ./.cabal-sandbox/bin"
+# Build and install to ./cabal-sandbox-bin (or ~/.cabal/bin without a sandbox)
+cabal install \
+  ./hledger-lib \
+  ./hledger \
+  ./hledger-ui \
+  ./hledger-web \
+  ./hledger-api \
