eac2ff0ea73c2e8cb132573318b9408a8a11a4f4
Simon Michael <simon@joyful.com>
2016-12-02 15:37:18 -0800

    ui: make --watch work on the error screen too

--- hledger-ui/Hledger/UI/ErrorScreen.hs
+++ hledger-ui/Hledger/UI/ErrorScreen.hs
@@ -92,7 +92,7 @@ esHandle ui@UIState{
             (pos,f) = case parsewithString hledgerparseerrorpositionp esError of
                         Right (f,l,c) -> (Just (l, Just c),f)
                         Left  _       -> (endPos, journalFilePath j)
-        e | e `elem` [VtyEvent (EvKey (KChar 'g') [])] ->
+        e | e `elem` [VtyEvent (EvKey (KChar 'g') []), AppEvent FileChange] ->
           liftIO (uiReloadJournal copts d (popScreen ui)) >>= continue . uiCheckBalanceAssertions d
 --           (ej, _) <- liftIO $ journalReloadIfChanged copts d j
 --           case ej of
