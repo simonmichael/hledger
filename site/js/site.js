2321c64327c628a80eeadcd006950e0b54b012d6
Simon Michael <simon@joyful.com>
2017-01-03 11:28:30 -0800

    doc: fix current version highlighting in web manuals

--- site/js/site.js
+++ site/js/site.js
@@ -6,7 +6,7 @@ function highlightDocVersion() {
   $('.versions').each( function() {
     var parts = window.location.pathname.split('/');
     var dir = parts.length > 1 ? parts[parts.length-2] : '';
-    var ver = $.isNumeric(dir) ? dir : '1.0';
+    var ver = $.isNumeric(dir) ? dir : '1.1';
     $(this).find('a').each( function() {
       if ($(this).html() == ver)
         $(this)
