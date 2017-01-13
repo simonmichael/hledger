f6b57589e5b914f355b6390b85449e72c8c804dc
Mykola Orliuk <virkony@gmail.com>
2017-01-13 14:16:41 +0200

    rewrite: rewrite every posting
    Also start using ModifierTransaction

--- bin/hledger-rewrite.hs
+++ bin/hledger-rewrite.hs
@@ -5,6 +5,7 @@
   --package megaparsec
   --package text
 -}
+{-# LANGUAGE LambdaCase #-}
 {-
 
 hledger-rewrite [PATTERNS] --add-posting "ACCT  AMTEXPR" ...
@@ -27,7 +28,6 @@ Related: https://github.com/simonmichael/hledger/issues/99
 TODO:
 - should allow regex matching and interpolating matched name in replacement
 - should apply all matching rules to a transaction, not just one
-- should apply the rule for each matched posting within a transaction, if there's more than one
 - should be possible to use this on unbalanced entries, eg while editing one
 
 -}
@@ -57,6 +57,17 @@ cmdmode = (defCommandMode ["hledger-rewrite"]) {
     }
   }
 
+modifierTransactionFromOpts :: RawOpts -> ModifierTransaction
+modifierTransactionFromOpts opts = txn where
+    exprs = addPostingExprsFromOpts opts :: [PostingExpr]
+    postings = flip map exprs $ \case
+        (acct, AmountLiteral s) -> acct `post'`  amountp' s
+        (acct, AmountMultiplier n) -> acct `post'` amount { acommodity = T.pack "*", aquantity = n }
+    txn = ModifierTransaction { mtvalueexpr = T.empty, mtpostings = postings }
+
+post' :: AccountName -> Amount -> Posting
+post' acct amt = (accountNameWithoutPostingType acct `post` amt) { ptype = accountNamePostingType acct }
+
 type PostingExpr = (AccountName, AmountExpr)
 
 data AmountExpr = AmountLiteral String | AmountMultiplier Quantity deriving (Show)
@@ -80,32 +91,32 @@ amountexprp =
     ,AmountLiteral <$> many anyChar
     ]
 
-amountExprRenderer :: Query -> AmountExpr -> (Transaction -> MixedAmount)
-amountExprRenderer q aex =
-  case aex of
-    AmountLiteral s    -> const (mamountp' s)
-    AmountMultiplier n -> (`divideMixedAmount` (1 / n)) . (`firstAmountMatching` q)
-  where
-    firstAmountMatching :: Transaction -> Query -> MixedAmount
-    firstAmountMatching t q = pamount $ head $ filter (q `matchesPosting`) $ tpostings t
 
-rewriteTransaction :: Transaction -> [(AccountName, Transaction -> MixedAmount)] -> Transaction
-rewriteTransaction t addps = t{tpostings=tpostings t ++ map (uncurry (generatePosting t)) addps}
-  where
-    generatePosting :: Transaction -> AccountName -> (Transaction -> MixedAmount) -> Posting
-    generatePosting t acct amtfn = nullposting{paccount     = accountNameWithoutPostingType acct
-                                              ,ptype        = accountNamePostingType acct
-                                              ,pamount      = amtfn t
-                                              ,ptransaction = Just t
-                                              }
+postingScale :: Posting -> Maybe Quantity
+postingScale p =
+    case amounts $ pamount p of
+        [a] | acommodity a == T.pack "*" -> Just $ aquantity a
+        _ -> Nothing
+
+runModifierPosting :: Posting -> (Posting -> Posting)
+runModifierPosting p' =
+    case postingScale p' of
+        Nothing -> \p -> p' { ptransaction = ptransaction p }
+        Just n -> \p -> p' { pamount = pamount p `divideMixedAmount` (1/n), ptransaction = ptransaction p }
+
+runModifierTransaction :: Query -> ModifierTransaction -> (Transaction -> Transaction)
+runModifierTransaction q mod = modifier where
+    mods = map runModifierPosting $ mtpostings mod
+    generatePostings ps = [mod p | p <- ps, q `matchesPosting` p, mod <- mods]
+    modifier t@Transaction{ tpostings = ps } = t { tpostings = ps ++ generatePostings ps }
 
 main = do
   opts@CliOpts{rawopts_=rawopts,reportopts_=ropts} <- getCliOpts cmdmode
   d <- getCurrentDay
   let q = queryFromOpts d ropts
-      addps = [(a, amountExprRenderer q aex) | (a, aex) <- addPostingExprsFromOpts rawopts]
+      mod = modifierTransactionFromOpts rawopts
   withJournalDo opts $ \opts j@Journal{jtxns=ts} -> do
     -- rewrite matched transactions
-    let j' = j{jtxns=map (\t -> if q `matchesTransaction` t then rewriteTransaction t addps else t) ts}
+    let j' = j{jtxns=map (runModifierTransaction q mod) ts}
     -- run the print command, showing all transactions
     print' opts{reportopts_=ropts{query_=""}} j'
