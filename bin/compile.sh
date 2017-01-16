9a1128e27639154155f89c6236e89b26cf479ea9
Simon Michael <simon@joyful.com>
2017-01-16 14:25:10 -0800

    addons: in bin/compile.sh, ensure hledger-chart's extra deps are installed

--- bin/compile.sh
+++ bin/compile.sh
@@ -2,6 +2,7 @@
 cd "$(dirname "$0")"
 echo "building dependencies"
 stack build hledger
+stack install Chart Chart-diagrams colour  # additional deps for hledger-chart
 echo "building add-on commands"
 for f in hledger-*.hs; do stack ghc $f; done
 echo "add-on commands available:"
