1218ca55f085aaa530b4999f8ccd2cd7473796c8
Simon Michael <simon@joyful.com>
2017-01-22 15:02:49 -0800

    bin: don't list .dyn_* files after compile.sh on mac

--- bin/compile.sh
+++ bin/compile.sh
@@ -6,4 +6,4 @@ stack install Chart Chart-diagrams colour  # additional deps for hledger-chart
 echo "building add-on commands"
 for f in hledger-*.hs; do stack ghc $f; done
 echo "add-on commands available:"
-ls -F hledger-* | grep -vE '\.(hs|hi|o)'
+ls -F hledger-* | grep -vE '\.(hs|hi|o|dyn_hi|dyn_o)'
