3564c929e138994df12233cca4dc8befb43f10b6
Simon Michael <simon@joyful.com>
2017-02-02 11:48:09 -0800

    addons: fix compilation of rewrite by bin/compile.sh

--- bin/compile.sh
+++ bin/compile.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")"
 echo "building dependencies"
 stack build hledger
-stack install Chart Chart-diagrams colour here  # additional deps needed by addons
+stack install Chart Chart-diagrams colour Diff here  # additional deps needed by addons
 echo "building add-on commands"
 for f in hledger-*.hs; do stack ghc $f; done
 echo "add-on commands available:"
