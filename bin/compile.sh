a4a3039fe9b0c4f9ff90238bab1e1f7411926a0a
Simon Michael <simon@joyful.com>
2017-03-22 14:18:29 +0000

    travis: require warning-free builds of addons #520

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# helper script, compiles all addons in this directory
+# Run this script to compile all addons in this directory.
 cd "$(dirname "$0")"
 echo "building dependencies"
 stack build hledger
 stack install Chart Chart-diagrams colour Diff here  # additional deps needed by addons
 echo "building add-on commands"
-for f in hledger-*.hs; do stack ghc $f; done
+for f in hledger-*.hs; do stack ghc -- -Wall -Werror $f; done
 echo "add-on commands available:"
 ls -F hledger-* | grep -vE '\.(hs|hi|o|dyn_hi|dyn_o)'
