af11cd6243b91acb3c580f29acb8ac478b5bf15c
Simon Michael <simon@joyful.com>
2017-06-06 09:21:51 -0700

    bump package versions to 1.2.98 (1.3 alpha)

--- hledger-api/hledger-api.hs
+++ hledger-api/hledger-api.hs
@@ -35,12 +35,12 @@ import           Text.Printf
 import Hledger.Query
 import Hledger.Cli hiding (Reader, version)
 
-hledgerApiVersion="1.2"
+hledgerApiVersion="1.2.98"
 
 -- https://github.com/docopt/docopt.hs#readme
 doc :: Docopt
 doc = [docopt|
-hledger-api 1.2
+hledger-api 1.2.98
 
 Serves hledger data and reports as a JSON web API.
 
