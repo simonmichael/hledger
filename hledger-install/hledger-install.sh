9b6bad20a4ff75a78c2570926de134b6f32b2148
Simon Michael <simon@joyful.com>
2017-07-20 08:20:02 +0100

    install: resolver notes
    [ci skip]

--- hledger-install/hledger-install.sh
+++ hledger-install/hledger-install.sh
@@ -31,7 +31,11 @@ HERE
 }
 
 VERSION=20170720
-RESOLVER="--resolver lts-8"
+RESOLVER="--resolver lts-8" 
+  # ^ lts-8 selects the latest lts-8.x snapshot for stack. You can specify another, 
+  # or comment out this line to use your current global resolver, which might 
+  # avoid some unnecessary building. lts-6 or 7 probably require tweaking the 
+  # hledger install commands below. OSX Sierra+ requires at least lts-8.0+. 
 HLEDGER_VERSION=1.3
 HLEDGER_DIFF_VERSION=0.2.0.9
 HLEDGER_IADD_VERSION=1.2.2
