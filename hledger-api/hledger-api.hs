5706353881543e75a19219aa9ac68d6461b94479
Simon Michael <simon@joyful.com>
2017-07-31 14:02:35 -0700

    bump version strings & lower bounds to 1.3.99

--- hledger-api/hledger-api.hs
+++ hledger-api/hledger-api.hs
@@ -35,12 +35,12 @@ import           Text.Printf
 import Hledger.Query
 import Hledger.Cli hiding (Reader, version)
 
-hledgerApiVersion="1.3"
+hledgerApiVersion="1.3.99"
 
 -- https://github.com/docopt/docopt.hs#readme
 doc :: Docopt
 doc = [docopt|
-hledger-api 1.3
+hledger-api 1.3.99
 
 Serves hledger data and reports as a JSON web API.
 
