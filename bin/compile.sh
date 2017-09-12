5e4673bd1f4e1b41943557015316d7c30573dde3
Simon Michael <simon@joyful.com>
2017-09-11 18:29:36 -0700

    bin: chart: rename script, skip deps to avoid breaking func tests
    hledger-chart's deps are not currently GHC 8.2 compatible

--- bin/compile.sh
+++ bin/compile.sh
@@ -3,7 +3,8 @@
 cd "$(dirname "$0")"
 echo "building dependencies"
 stack build hledger
-stack install Chart Chart-diagrams colour Diff here  # additional deps needed by addons
+# additional deps needed by addons
+stack install Diff here #Chart Chart-diagrams colour 
 echo "building add-on commands"
 for f in hledger-*.hs; do stack ghc -- -Wall -Werror $f; done
 echo "add-on commands available:"
