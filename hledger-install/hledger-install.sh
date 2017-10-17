bf234431ef144355f7b39806db6c27d7878d8732
Simon Michael <simon@joyful.com>
2017-10-16 20:31:02 -0700

    install: bump version
    [ci skip]

--- hledger-install/hledger-install.sh
+++ hledger-install/hledger-install.sh
@@ -46,7 +46,7 @@ HERE
 HLEDGER_INSTALL_TOOL=hledger-install.sh
   # ^ this script's name (can't use $0 when it's piped into bash)
 
-HLEDGER_INSTALL_VERSION=20171005
+HLEDGER_INSTALL_VERSION=20171016
 
 RESOLVER="--resolver=lts-9"
   # ^ lts-9 selects the latest lts-9.x snapshot from stackage. You can specify another, 
