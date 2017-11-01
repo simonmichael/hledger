49090d9e53f51ca89f41108e2885bc37f299153a
Simon Michael <simon@joyful.com>
2017-11-01 08:05:17 -0700

    bin: stop listing addons after compiling them

--- bin/compile.sh
+++ bin/compile.sh
@@ -7,5 +7,3 @@ stack build hledger
 stack install Diff here #Chart Chart-diagrams colour 
 echo "building add-on commands"
 for f in hledger-*.hs; do stack ghc -- -Wall -Werror $f; done
-echo "add-on commands available:"
-ls -F hledger-* | grep -vE '\.(hs|hi|o|dyn_hi|dyn_o)'
