1a602de4e65f6c0a03840fab1a68211343d85491
Dmitry Astapov <dastapov@gmail.com>
2017-11-18 00:40:10 +0000

    added --forecast to balancemode, print, register

--- hledger/Hledger/Cli/CliOptions.hs
+++ hledger/Hledger/Cli/CliOptions.hs
@@ -155,6 +155,7 @@ reportflags = [
  ,flagNone ["empty","E"]     (setboolopt "empty") "show items with zero amount, normally hidden"
  ,flagNone ["cost","B"]      (setboolopt "cost") "convert amounts to their cost at transaction time (using the transaction price, if any)"
  ,flagNone ["value","V"]     (setboolopt "value") "convert amounts to their market value on the report end date (using the most recent applicable market price, if any)"
+ ,flagNone ["forecast"] (\opts -> setboolopt "forecast" opts) "generate forecast transactions"
  ]
 
 -- | Common output-related flags: --output-file, --output-format...
