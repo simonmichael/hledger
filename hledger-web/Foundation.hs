e368c99900fe180d79484ddadd493ba605adf565
Simon Michael <simon@joyful.com>
2018-03-12 03:38:15 +0000

    web: fix outdated api usage to support yesod 1.6 (#717)

--- hledger-web/Foundation.hs
+++ hledger-web/Foundation.hs
@@ -150,11 +150,12 @@ instance Yesod App where
         staticRootUrl <- (staticRoot . settings) <$> getYesod
         withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
+    -- TODO outdated, still needed ?
     -- This is done to provide an optimization for serving static files from
     -- a separate domain. Please see the staticRoot setting in Settings.hs
-    urlRenderOverride y (StaticR s) =
-        Just $ uncurry (joinPath y (Settings.staticRoot $ settings y)) $ renderRoute s
-    urlRenderOverride _ _ = Nothing
+    -- urlRenderOverride y (StaticR s) =
+    --     Just $ uncurry (joinPath y (Settings.staticRoot $ settings y)) $ renderRoute s
+    urlParamRenderOverride _ _ _ = Nothing
 
 #ifndef DEVELOPMENT
     -- This function creates static content files in the static folder
@@ -166,11 +167,6 @@ instance Yesod App where
 
     -- Place Javascript at bottom of the body tag so the rest of the page loads first
     jsLoader _ = BottomOfBody
-
-    -- What messages should be logged. The following includes all messages when
-    -- in development, and warnings and errors in production.
-    shouldLog _ _source level =
-        development || level == LevelWarn || level == LevelError
 
 -- This instance is required to use forms. You can modify renderMessage to
 -- achieve customized and internationalized form validation messages.
