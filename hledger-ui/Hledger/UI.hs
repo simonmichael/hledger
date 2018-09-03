0f83fb91c19b00649fb5070abf44d90a54b69071
Simon Michael <simon@joyful.com>
2018-09-03 08:43:57 -0700

    ui, web: fix build after tests cleanup

--- hledger-ui/Hledger/UI.hs
+++ hledger-ui/Hledger/UI.hs
@@ -5,12 +5,10 @@ Re-export the modules of the hledger-ui program.
 module Hledger.UI (
                      module Hledger.UI.Main,
                      module Hledger.UI.UIOptions,
-                     module Hledger.UI.Theme,
-                     tests_Hledger_UI
+                     module Hledger.UI.Theme
               )
 where
 
 import Hledger.UI.Main
 import Hledger.UI.UIOptions
 import Hledger.UI.Theme
-import Test.HUnit as U
--- hledger-web/Hledger/Web.hs
+++ hledger-web/Hledger/Web.hs
@@ -5,9 +5,7 @@ Re-export the modules of the hledger-web program.
 module Hledger.Web
   ( module Hledger.Web.WebOptions
   , module Hledger.Web.Main
-  , tests_Hledger_Web
   ) where
 
 import Hledger.Web.WebOptions
 import Hledger.Web.Main
-import Test.HUnit as U
