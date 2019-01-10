d0611a3fdd30ce5ea42979da7b893fb6fe82d6a4
Jakob Schöttl <jschoett@gmail.com>
2019-01-10 19:13:28 +0100

    Refactor helper script

--- shell-completion/output-commands.sh
+++ shell-completion/output-commands.sh
@@ -3,19 +3,14 @@
 
 set -o errexit -o pipefail -o nounset
 
-printCommands() {
-    declare tmp=$1
-    sed -rn 's/^ ([-a-z]+).*/\1/gp' "$tmp"
-    sed -rn 's/^ .*\(([a-z]+)\).*/\1/gp' "$tmp"
-    # TODO missing: (reg, r)  (multiple aliases)
-}
-
 main() {
     declare tmp
     tmp=$(mktemp)
     cat > "$tmp"
 
-    printCommands "$tmp" | grep -v ^hledger
+    sed -rn 's/^ ([-a-z]+).*/\1/gp' "$tmp"
+    sed -rn 's/^ .*\(([a-z]+)\).*/\1/gp' "$tmp"
+    # TODO missing: (reg, r)  (multiple aliases)
 }
 
 main "$@"
