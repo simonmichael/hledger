13c42357305bb008328490c178d316ba527dbf4a
Jakob Schöttl <jschoett@gmail.com>
2019-01-12 19:59:04 +0100

    Leave out subcommands with only one letter

--- shell-completion/output-commands.sh
+++ shell-completion/output-commands.sh
@@ -9,7 +9,11 @@ main() {
     cat > "$tmp"
 
     sed -rn 's/^ ([-a-z]+).*/\1/gp' "$tmp"
-    sed -rn 's/^ .*\(([a-z]+)\).*/\1/gp' "$tmp"
+
+    # Do not output single letter commands, it's not useful.
+    sed -rn 's/^ .*\(([a-z]+)\).*/\1/gp' "$tmp" \
+	| grep -v ^.$
+
     # TODO missing: (reg, r)  (multiple aliases)
 }
 
