e96dfe832fcd99abb1ec12617a6a15e03694711d
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2019-09-28 12:35:40 +0200

    web: Allow CORS to be enabled in hledger-web
    Add a --cors option to the CLI to enable simple cross-origin requests

--- hledger-web/Hledger/Web/Application.hs
+++ hledger-web/Hledger/Web/Application.hs
@@ -10,6 +10,7 @@ module Hledger.Web.Application
 
 import Data.IORef (newIORef, writeIORef)
 import Network.Wai.Middleware.RequestLogger (logStdoutDev, logStdout)
+import Network.Wai.Middleware.Cors
 import Network.HTTP.Client (defaultManagerSettings)
 import Network.HTTP.Conduit (newManager)
 import Yesod.Default.Config
@@ -23,7 +24,7 @@ import Hledger.Web.Handler.UploadR
 import Hledger.Web.Handler.JournalR
 import Hledger.Web.Handler.RegisterR
 import Hledger.Web.Import
-import Hledger.Web.WebOptions (WebOpts(serve_,serve_api_))
+import Hledger.Web.WebOptions (WebOpts(serve_,serve_api_,cors_))
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
@@ -38,11 +39,13 @@ makeApplication :: WebOpts -> Journal -> AppConfig DefaultEnv Extra -> IO Applic
 makeApplication opts' j' conf' = do
     foundation <- makeFoundation conf' opts'
     writeIORef (appJournal foundation) j'
-    logWare <$> toWaiApp foundation
+    (logWare . corsWare) <$> toWaiApp foundation
   where
     logWare | development  = logStdoutDev
             | serve_ opts' || serve_api_ opts' = logStdout
             | otherwise    = id
+    corsWare | cors_ opts' = simpleCors
+             | otherwise = id
 
 makeFoundation :: AppConfig DefaultEnv Extra -> WebOpts -> IO App
 makeFoundation conf opts' = do
