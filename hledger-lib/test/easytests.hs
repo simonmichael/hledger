f82ed83153655cc57f1b6059e1755c308c40245c
Mykola Orliuk <virkony@gmail.com>
2019-11-16 15:02:00 +0100

    ;lib: re-enable easytests

--- hledger-lib/test/easytests.hs
+++ hledger-lib/test/easytests.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE PackageImports #-}
 {-
 Run hledger-lib's easytest tests using the easytest runner.
+Note that we use package-qualified import to overcome
+Cabal heuristic missing-home-modules.
 -}
-import Hledger
+import "hledger-lib" Hledger
 main = run tests_Hledger
