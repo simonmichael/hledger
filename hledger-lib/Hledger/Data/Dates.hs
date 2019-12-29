35481a665b1c81fec6661bbaf9d0607c813bb702
Brian Wignall <brianwignall@gmail.com>
2019-12-29 17:27:01 -0500

    Change Date output to yyyy-mm-dd

--- hledger-lib/Hledger/Data/Dates.hs
+++ hledger-lib/Hledger/Data/Dates.hs
@@ -115,7 +115,7 @@ instance Show DateSpan where
     -- show s = "DateSpan \"" ++ showDateSpan s ++ "\"" -- quotes to help pretty-show
 
 showDate :: Day -> String
-showDate = formatTime defaultTimeLocale "%0C%y/%m/%d"
+showDate = show
 
 -- | Render a datespan as a display string, abbreviating into a
 -- compact form if possible.
