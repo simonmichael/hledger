5cdfc87be2db5c9d92518f73a1e359ed4eac83b1
Félix Sipma <felix.sipma@no-log.org>
2020-04-01 19:48:14 +0200

    use Revenue and Expense account names in incomestatement

--- hledger/Hledger/Cli/Commands/Incomestatement.hs
+++ hledger/Hledger/Cli/Commands/Incomestatement.hs
@@ -11,23 +11,26 @@ module Hledger.Cli.Commands.Incomestatement (
 ) where
 
 import System.Console.CmdArgs.Explicit
+import Data.Map as M
+import Data.Text as T
 
 import Hledger
 import Hledger.Cli.CliOptions
 import Hledger.Cli.CompoundBalanceCommand
+import Hledger.Data.Types
 
-incomestatementSpec = CompoundBalanceCommandSpec {
+incomestatementSpec j = CompoundBalanceCommandSpec {
   cbcdoc      = $(embedFileRelative "Hledger/Cli/Commands/Incomestatement.txt"),
   cbctitle    = "Income Statement",
   cbcqueries  = [
      CBCSubreportSpec{
-      cbcsubreporttitle="Revenues"
+      cbcsubreporttitle=T.unpack $ maybe "Revenues" (Prelude.head . accountNameComponents . Prelude.head) (M.lookup Revenue (jdeclaredaccounttypes j))
      ,cbcsubreportquery=journalRevenueAccountQuery
      ,cbcsubreportnormalsign=NormallyNegative
      ,cbcsubreportincreasestotal=True
      }
     ,CBCSubreportSpec{
-      cbcsubreporttitle="Expenses"
+      cbcsubreporttitle=T.unpack $ maybe "Expenses" (Prelude.head . accountNameComponents . Prelude.head) (M.lookup Expense (jdeclaredaccounttypes j))
      ,cbcsubreportquery=journalExpenseAccountQuery
      ,cbcsubreportnormalsign=NormallyPositive
      ,cbcsubreportincreasestotal=False
@@ -37,7 +40,7 @@ incomestatementSpec = CompoundBalanceCommandSpec {
 }
 
 incomestatementmode :: Mode RawOpts
-incomestatementmode = compoundBalanceCommandMode incomestatementSpec
+incomestatementmode = compoundBalanceCommandMode $ incomestatementSpec nulljournal
 
 incomestatement :: CliOpts -> Journal -> IO ()
-incomestatement = compoundBalanceCommand incomestatementSpec
+incomestatement opts j = compoundBalanceCommand (incomestatementSpec j) opts j
