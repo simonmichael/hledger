1f2cd8ec2eca747dc8ad0d273284bea540e30972
Simon Michael <simon@joyful.com>
2020-06-06 17:26:05 -0700

    ;install: bump versions

--- hledger-install/hledger-install.sh
+++ hledger-install/hledger-install.sh
@@ -50,7 +50,7 @@ HERE
 HLEDGER_INSTALL_TOOL=hledger-install.sh
 
 # this script's version
-HLEDGER_INSTALL_VERSION=202003192
+HLEDGER_INSTALL_VERSION=20200606
 
 # stackage snapshot to use when installing with stack.
 # You can try specifying a different stackage version here, or 
@@ -72,12 +72,12 @@ hledger-interest \
 "
 
 # latest hledger package versions; update often:
-HLEDGER_LIB_VERSION=1.17.1
-HLEDGER_VERSION=1.17.1.1
-HLEDGER_UI_VERSION=1.17.1.1
-HLEDGER_WEB_VERSION=1.17.1
+HLEDGER_LIB_VERSION=1.18
+HLEDGER_VERSION=1.18
+HLEDGER_UI_VERSION=1.18
+HLEDGER_WEB_VERSION=1.18
 
-HLEDGER_IADD_VERSION=1.3.10
+HLEDGER_IADD_VERSION=1.3.11
 HLEDGER_INTEREST_VERSION=1.5.4
 
 # extra dependencies that aren't in stackage:
