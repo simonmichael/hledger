0e0964cc8982afd64450203c3ab068a005a6ccb4
Stephen Morgan <morgan.stephen99@gmail.com>
2020-08-10 11:13:01 +1000

    cli: Replace use of fromIntegral in anon with unsafeCoerce. Much safer!

--- hledger/Hledger/Cli/Anon.hs
+++ hledger/Hledger/Cli/Anon.hs
@@ -5,6 +5,7 @@ Instances for anonymizing sensitive data in various types.
 Note that there is no clear way to anonymize numbers.
 
 -}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Hledger.Cli.Anon
     ( Anon(..)
@@ -14,9 +15,11 @@ where
 
 import Control.Arrow (first)
 import Data.Hashable (hash)
-import Data.Word (Word32)
-import Numeric (showHex)
 import qualified Data.Text as T
+import Data.Text.Lazy (toStrict)
+import Data.Text.Lazy.Builder (toLazyText)
+import Data.Text.Lazy.Builder.Int (hexadecimal)
+import Unsafe.Coerce (unsafeCoerce)
 
 import Hledger.Data
 
@@ -26,16 +29,16 @@ class Anon a where
 
 instance Anon Journal where
     -- Apply the anonymisation transformation on a journal after finalisation
-    anon j = j { jtxns = map anon . jtxns $ j
+    anon j = j { jtxns = map anon $ jtxns j
                , jparseparentaccounts = map anonAccount $ jparseparentaccounts j
                , jparsealiases = []  -- already applied
                , jdeclaredaccounts = map (first anon) $ jdeclaredaccounts j
                }
 
 instance Anon Posting where
-    anon p = p { paccount = anonAccount . paccount $ p
-               , pcomment = T.empty
-               , ptransaction = fmap anon . ptransaction $ p  -- Note that this will be overridden
+    anon p = p { paccount = anonAccount $ paccount p
+               , pcomment = ""
+               , ptransaction = anon <$> ptransaction p  -- Note that this will be overridden
                , poriginal = anon <$> poriginal p
                }
 
@@ -48,6 +51,7 @@ instance Anon Transaction where
 
 -- | Anonymize account name preserving hierarchy
 anonAccount :: AccountName -> AccountName
-anonAccount = T.intercalate (T.pack ":") . map anon . T.splitOn (T.pack ":")
+anonAccount = T.intercalate ":" . map anon . T.split (==acctsepchar)
 
-instance Anon T.Text where anon = T.pack . flip showHex "" . (fromIntegral :: Int -> Word32) . hash
+instance Anon T.Text where
+  anon = toStrict . toLazyText . hexadecimal . (unsafeCoerce :: Int -> Word) . hash
