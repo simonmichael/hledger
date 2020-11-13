c7e267e314e7ee9d1736d7c48087ad06439f2dc8
Simon Michael <simon@joyful.com>
2020-11-13 09:36:17 -0800

    ;web: begin work on a forecasting test (#1390)

--- hledger-web/test/test.hs
+++ hledger-web/test/test.hs
@@ -10,17 +10,33 @@ module Main where
 import Test.Hspec (hspec)
 import Yesod.Default.Config
 import Yesod.Test
+
 import Hledger.Web.Application (makeFoundation)
 import Hledger.Web.Foundation
 import Hledger.Web.Settings (parseExtra)
-import Hledger.Web.WebOptions (defwebopts)
+import Hledger.Web.WebOptions -- (defwebopts, cliopts_)
+-- import Hledger.Cli.CliOptions -- (defcliopts, reportspec_)
+-- import Hledger  -- .Reports.ReportOptions (defreportopts, forecast_)
 
 main :: IO ()
 main = do
   conf <- Yesod.Default.Config.loadConfig $ 
             (configSettings Testing){ csParseExtra = parseExtra }
+
   foundation <- makeFoundation conf defwebopts
   hspec $ yesodSpec foundation specs
+
+  -- run hledger-web with some forecasted transactions
+  -- XXX problem: these tests use makeFoundation, bypassing the journal setup in Hledger.Web.Main
+  -- d <- getCurrentDay
+  -- let
+  --   ropts = defreportopts{forecast_=Just nulldatespan}
+  --   rspec = case reportOptsToSpec d ropts of
+  --             Left e   -> error $ "failed to set up report options for tests, shouldn't happen: " ++ show e
+  --             Right rs -> rs
+  -- foundationForecast <- makeFoundation conf 
+  --   defwebopts{cliopts_=defcliopts{file_=["hledger-web/tests/forecast.j"], reportspec_=rspec}}
+  -- hspec $ yesodSpec foundationForecast specsForecast
 
 -- https://hackage.haskell.org/package/yesod-test/docs/Yesod-Test.html
 
@@ -38,13 +54,22 @@ specs = do
       statusIs 200
       bodyContains "accounts"
 
-      -- post "/" $ do
-      --   addNonce
-      --   fileByLabel "Choose a file" "tests/main.hs" "text/plain" -- talk about self-reference
-      --   byLabel "What's on the file?" "Some Content"
+-- specsForecast :: YesodSpec App
+-- specsForecast = do
+--   ydescribe "hledger-web --forecast" $ do
+--     yit "serves a reasonable-looking journal page" $ do
+--       get JournalR
+--       statusIs 200
+--       bodyContains "Add a transaction"
 
-      -- statusIs 200
-      -- htmlCount ".message" 1
-      -- htmlAllContain ".message" "Some Content"
-      -- htmlAllContain ".message" "text/plain"
 
+
+
+-- post "/" $ do
+--   addNonce
+--   fileByLabel "Choose a file" "tests/main.hs" "text/plain" -- talk about self-reference
+--   byLabel "What's on the file?" "Some Content"
+-- statusIs 200
+-- htmlCount ".message" 1
+-- htmlAllContain ".message" "Some Content"
+-- htmlAllContain ".message" "text/plain"
