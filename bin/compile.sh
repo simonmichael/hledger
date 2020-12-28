9106ac2fa8f4db1beb16e9f1cfdadcba12b33e0e
Simon Michael <simon@joyful.com>
2020-12-28 12:44:53 -0800

    ci: also test compilation of bin/ add-on scripts
    (cherry picked from commit 06b466d847c46dc384cdef95658dade68111b173)

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-# Run this script (or "make addons") to compile all addons in this directory.
-cd "$(dirname "$0")"
-echo "building dependencies"
+# Compile all add-on scrips in this directory.
+
+cd "$(dirname "$0")" || exit
+
+echo "building hledger libraries"
 stack build hledger
-# additional deps needed by addons
-stack install Diff here #Chart Chart-diagrams colour 
-echo "building add-on commands"
-for f in hledger-*.hs; do stack ghc -- -Wall -Werror $f; done
+
+echo "installing extra libraries needed by scripts"
+stack install string-qq
+
+echo "compiling hledger-* scripts"
+for f in hledger-*.hs; do stack ghc -- "$f"; done
+  # stack script --compile would install extra libs more automatically
+  # but would also run scripts, which we don't want
