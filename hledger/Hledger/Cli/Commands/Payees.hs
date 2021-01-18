540c65994c65c909bf1e365aa0c05671636ac1c7
Simon Michael <simon@joyful.com>
2021-01-17 16:19:22 -0800

    payees: add --used/--declared flags, like accounts

--- hledger/Hledger/Cli/Commands/Payees.hs
+++ hledger/Hledger/Cli/Commands/Payees.hs
@@ -17,6 +17,7 @@ module Hledger.Cli.Commands.Payees (
 
 import Data.List.Extra (nubSort)
 import qualified Data.Text.IO as T
+import System.Console.CmdArgs.Explicit as C
 
 import Hledger
 import Hledger.Cli.CliOptions
@@ -25,14 +26,24 @@ import Hledger.Cli.CliOptions
 -- | Command line options for this command.
 payeesmode = hledgerCommandMode
   $(embedFileRelative "Hledger/Cli/Commands/Payees.txt")
-  []
+  [flagNone ["declared"] (setboolopt "declared") "show payees declared with payee directives"
+  ,flagNone ["used"] (setboolopt "used") "show payees referenced by transactions"
+  ]
   [generalflagsgroup1]
   hiddenflags
   ([], Just $ argsFlag "[QUERY]")
 
 -- | The payees command.
 payees :: CliOpts -> Journal -> IO ()
-payees CliOpts{reportspec_=rspec} j = do
-  let ts = entriesReport rspec j
-      payees = nubSort $ map transactionPayee ts
+payees CliOpts{rawopts_=rawopts, reportspec_=ReportSpec{rsQuery=query}} j = do
+  let 
+    declared = boolopt "declared" rawopts
+    used     = boolopt "used"     rawopts
+    -- XXX matchesPayee is currently an alias for matchesDescription, not sure if it matters
+    matcheddeclaredpayees = filter (matchesPayeeWIP query) $ journalPayeesDeclared j
+    matchedusedpayees     = map transactionPayee $ filter (matchesTransaction query) $ jtxns j
+    payees = nubSort $
+      if | declared     && not used -> matcheddeclaredpayees
+         | not declared && used     -> matchedusedpayees
+         | otherwise                -> matcheddeclaredpayees ++ matchedusedpayees
   mapM_ T.putStrLn payees
--- hledger/Hledger/Cli/Commands/Payees.md
+++ hledger/Hledger/Cli/Commands/Payees.md
@@ -3,11 +3,16 @@ List the unique payee/payer names that appear in transactions.
 
 _FLAGS
 
-This command lists the unique payee/payer names that appear in transactions,
-in alphabetic order. 
-You can add a query to select a subset of transactions.
-The payee/payer is the part of the transaction description before a | character
+This command lists unique payee/payer names which have been 
+declared with payee directives (--declared), 
+used in transaction descriptions (--used), 
+or both (the default).
+
+The payee/payer is the part of the transaction description before a | character 
 (or if there is no |, the whole description).
+
+You can add query arguments to select a subset of transactions. This implies --used.
+
 
 Example:
 ```shell
