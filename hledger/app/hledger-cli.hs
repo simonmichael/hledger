c83970422cbe50911fe713e8ab2d7d0606d1069a
Simon Michael <simon@joyful.com>
2021-03-12 06:59:43 -0800

    cli: fix a build failure with ghc 9.0 (#1503)
    Also remove the obsolete shebang line.

--- hledger/app/hledger-cli.hs
+++ hledger/app/hledger-cli.hs
@@ -1,6 +1,9 @@
-#!/usr/bin/env runhaskell
 -- the hledger command-line executable; see Hledger/Cli/Main.hs
 
 module Main (main)
 where
-import Hledger.Cli.Main (main)
+import qualified Hledger.Cli.Main (main)
+
+-- Have to write this explicitly for GHC 9.0.1a for some reason:
+main :: IO ()
+main = Hledger.Cli.Main.main
