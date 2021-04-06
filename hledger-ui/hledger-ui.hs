4578d5d32951ae3ef22c2445fe75c43b8f845a5d
Simon Michael <simon@joyful.com>
2021-04-06 07:45:49 -1000

    ui: work around ghc 9.0 build failure (#1503)
    Also remove obsolete shebang line.

--- hledger-ui/hledger-ui.hs
+++ hledger-ui/hledger-ui.hs
@@ -1,5 +1,7 @@
-#!/usr/bin/env runhaskell
-module Main
-  (module Hledger.UI)
+module Main (main)
 where
-import Hledger.UI (main)
+-- import Hledger.UI (main)
+-- workaround for GHC 9.0.1 https://gitlab.haskell.org/ghc/ghc/-/issues/19397, #1503
+import qualified Hledger.UI.Main (main)
+main :: IO ()
+main = Hledger.UI.Main.main
