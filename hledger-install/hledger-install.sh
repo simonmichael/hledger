fa85eab16134dbf212cb997b5911442c05fe315e
Simon Michael <simon@joyful.com>
2021-07-08 10:52:47 -1000

    ;install: hledger-iadd 1.3.15

--- hledger-install/hledger-install.sh
+++ hledger-install/hledger-install.sh
@@ -46,7 +46,7 @@ HERE
 HLEDGER_INSTALL_TOOL=hledger-install.sh
 
 # this script's version
-HLEDGER_INSTALL_VERSION=20210703
+HLEDGER_INSTALL_VERSION=20210708
 
 # stackage snapshot to use when installing with stack.
 # You can try specifying a different stackage version here, or 
@@ -74,7 +74,7 @@ HLEDGER_VERSION=1.22
 HLEDGER_UI_VERSION=1.22
 HLEDGER_WEB_VERSION=1.22
 # addons:
-HLEDGER_IADD_VERSION=1.3.14
+HLEDGER_IADD_VERSION=1.3.15
 HLEDGER_INTEREST_VERSION=1.6.1
 
 # any required dependencies that aren't in the stackage resolver above:
