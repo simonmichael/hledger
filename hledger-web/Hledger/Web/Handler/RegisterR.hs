f3f821b3ee922d1e1c6e9f7a7ae2ac594b8db50a
Stephen Morgan <morgan.stephen99@gmail.com>
2021-07-09 15:50:28 +1000

    fix: web: Make sure to strip prices from amounts before displaying in hledger-web chart. (#1597)

--- hledger-web/Hledger/Web/Handler/RegisterR.hs
+++ hledger-web/Hledger/Web/Handler/RegisterR.hs
@@ -108,6 +108,7 @@ registerChartHtml q title percommoditytxnreports = $(hamletFile "templates/chart
    colorForCommodity = fromMaybe 0 . flip lookup commoditiesIndex
    commoditiesIndex = zip (map fst percommoditytxnreports) [0..] :: [(CommoditySymbol,Int)]
    simpleMixedAmountQuantity = maybe 0 aquantity . listToMaybe . amounts
+   showZeroCommodity = wbUnpack . showMixedAmountB oneLine{displayZeroCommodity=True}
    shownull c = if null c then " " else c
    nodatelink = (RegisterR, [("q", T.unwords $ removeDates q)])
 
