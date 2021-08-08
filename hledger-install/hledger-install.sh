691817868472da1106d481127b2abb9e94ae82aa
Simon Michael <simon@joyful.com>
2021-08-08 07:47:07 -1000

    install: 1.22.2

--- hledger-install/hledger-install.sh
+++ hledger-install/hledger-install.sh
@@ -46,7 +46,7 @@ HERE
 HLEDGER_INSTALL_TOOL=hledger-install.sh
 
 # this script's version
-HLEDGER_INSTALL_VERSION=20210802
+HLEDGER_INSTALL_VERSION=20210808
 
 # stackage snapshot to use when installing with stack.
 # You can try specifying a different stackage version here, or 
@@ -69,10 +69,10 @@ hledger-interest \
 
 # Latest hledger package versions.
 # Don't forget to also bump HLEDGER_INSTALL_VERSION above.
-HLEDGER_LIB_VERSION=1.22.1
-HLEDGER_VERSION=1.22.1
-HLEDGER_UI_VERSION=1.22.1
-HLEDGER_WEB_VERSION=1.22.1
+HLEDGER_LIB_VERSION=1.22.2
+HLEDGER_VERSION=1.22.2
+HLEDGER_UI_VERSION=1.22.2
+HLEDGER_WEB_VERSION=1.22.2
 # addons:
 HLEDGER_IADD_VERSION=1.3.15
 HLEDGER_INTEREST_VERSION=1.6.1
@@ -82,6 +82,7 @@ EXTRA_DEPS="\
 "
 
 # the oldest version of stack that might possibly work:
+# XXX 2.3.1 is doubtful, retest; 2.5.1 ?
 STACK_MIN_VERSION=2.3.1
 
 
