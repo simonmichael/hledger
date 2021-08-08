bd113b223f77531e7517f38f771d8183ba994245
Stephen Morgan <morgan.stephen99@gmail.com>
2021-08-08 18:38:47 +1000

    dev: lens: Introduce lenses for CliOpts.

--- hledger/Hledger/Cli/CliOptions.hs
+++ hledger/Hledger/Cli/CliOptions.hs
@@ -5,7 +5,13 @@ related utilities used by hledger commands.
 
 -}
 
-{-# LANGUAGE CPP, ScopedTypeVariables, FlexibleContexts, TypeFamilies, OverloadedStrings, PackageImports #-}
+{-# LANGUAGE CPP                 #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE PackageImports      #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeFamilies        #-}
 
 module Hledger.Cli.CliOptions (
 
@@ -35,6 +41,7 @@ module Hledger.Cli.CliOptions (
 
   -- * CLI options
   CliOpts(..),
+  HasCliOpts(..),
   defcliopts,
   getHledgerCliOpts,
   getHledgerCliOpts',
@@ -747,3 +754,22 @@ getDirectoryContentsSafe d =
 --     putStrLn $ "processed opts:\n" ++ show opts
 --     d <- getCurrentDay
 --     putStrLn $ "search query: " ++ (show $ queryFromOpts d $ reportopts_ opts)
+
+-- ** Lenses
+
+makeHledgerClassyLenses ''CliOpts
+
+instance HasInputOpts CliOpts where
+    inputOpts = inputopts
+
+instance HasBalancingOpts CliOpts where
+    balancingOpts = inputOpts.balancingOpts
+
+instance HasReportSpec CliOpts where
+    reportSpec = reportspec
+
+instance HasReportOptsNoUpdate CliOpts where
+    reportOptsNoUpdate = reportSpec.reportOptsNoUpdate
+
+instance HasReportOpts CliOpts where
+    reportOpts = reportSpec.reportOpts
