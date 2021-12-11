9ca681097a4f1cc236223b505171fed3b49643cf
Simon Michael <simon@joyful.com>
2021-12-10 16:44:18 -1000

    install: 1.24.1

--- hledger-install/hledger-install.sh
+++ hledger-install/hledger-install.sh
@@ -46,13 +46,13 @@ HERE
 HLEDGER_INSTALL_TOOL=hledger-install.sh
 
 # this script's version
-HLEDGER_INSTALL_VERSION=20211201
+HLEDGER_INSTALL_VERSION=20211210
 
 # stackage snapshot to use when installing with stack.
 # You can try specifying a different stackage version here, or 
 # commenting out this line to use your current global resolver,
 # to avoid unnecessary building.
-RESOLVER="--resolver=lts-18.10"
+RESOLVER="--resolver=lts-18.18"
 
 # things to be installed
 
@@ -69,10 +69,10 @@ hledger-interest \
 
 # Latest hledger package versions.
 # Don't forget to also bump HLEDGER_INSTALL_VERSION above.
-HLEDGER_LIB_VERSION=1.24
-HLEDGER_VERSION=1.24
-HLEDGER_UI_VERSION=1.24
-HLEDGER_WEB_VERSION=1.24
+HLEDGER_LIB_VERSION=1.24.1
+HLEDGER_VERSION=1.24.1
+HLEDGER_UI_VERSION=1.24.1
+HLEDGER_WEB_VERSION=1.24.1
 # addons:
 HLEDGER_IADD_VERSION=1.3.16
 HLEDGER_INTEREST_VERSION=1.6.3
