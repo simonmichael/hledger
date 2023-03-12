64b57da01a476bdb38aede3515855a8abd4d6077
Simon Michael <simon@joyful.com>
2023-03-11 14:08:39 -1000

    ;install: bump resolver

--- hledger-install/hledger-install.sh
+++ hledger-install/hledger-install.sh
@@ -86,7 +86,7 @@ STACK_MIN_VERSION=2.5.1
 # You can try specifying a different stackage version here, or 
 # commenting out this line to use your current global resolver,
 # to avoid unnecessary building.
-STACK_RESOLVER="--resolver=lts-20.8"
+STACK_RESOLVER="--resolver=lts-20.13"
 
 # Dependencies we require that aren't in the above stackage snapshot.
 # (Also requested when using cabal, but that's harmless.)
