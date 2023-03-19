33876905f1d1104fc98e98135b72c4b5278bdd38
Simon Michael <simon@joyful.com>
2023-03-18 20:01:53 -1000

    dev: lib: Hledger.Cli.Script: cleanup

--- hledger/Hledger/Cli/Script.hs
+++ hledger/Hledger/Cli/Script.hs
@@ -6,34 +6,34 @@ aiming to provide the most useful imports and reduce boilerplate.
 {-# LANGUAGE PackageImports #-}
 
 module Hledger.Cli.Script
-( module Script
+( module M
 ) 
 where
 
-import Control.Applicative as Script
-import Control.Concurrent as Script
-import Control.Monad as Script
-import Data.Char as Script
-import Data.Either as Script
-import Data.Functor as Script
-import Data.List as Script
-import Data.Maybe as Script
-import Data.Ord as Script
+import Control.Applicative as M
+import Control.Concurrent as M
+import Control.Monad as M
+import Data.Char as M
+import Data.Either as M
+import Data.Functor as M
+import Data.List as M
+import Data.Maybe as M
+import Data.Ord as M
 -- import Data.String.QQ (s)  -- https://github.com/audreyt/string-qq/pull/3
-import Data.Time as Script
-import Text.Printf as Script hiding (formatString)
-import "text" Data.Text as Script (Text, pack, unpack)
+import Data.Time as M
+import Text.Printf as M hiding (formatString)
+import "text" Data.Text as M (Text, pack, unpack)
   -- can't re-export much of Data.Text & Data.Text.IO, they need to be qualified
-import Safe as Script hiding (at)
--- import qualified System.Console.CmdArgs.Explicit as Script
-import System.Directory as Script
-import System.Environment as Script
-import System.Exit as Script
-import System.FilePath as Script
-import System.IO as Script
-import System.IO.Error as Script
-import System.Process as Script
+import Safe as M hiding (at)
+-- import qualified System.Console.CmdArgs.Explicit as M
+import System.Directory as M
+import System.Environment as M
+import System.Exit as M
+import System.FilePath as M
+import System.IO as M
+import System.IO.Error as M
+import System.Process as M
 
-import Hledger as Script
-import Hledger.Cli as Script
-import Hledger.Cli.Main as Script (argsToCliOpts)
+import Hledger as M
+import Hledger.Cli as M
+import Hledger.Cli.Main as M (argsToCliOpts)
