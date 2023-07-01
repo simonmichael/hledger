e3bf66755a7b26e4fa0cb602f677528bc2f33593
Simon Michael <simon@joyful.com>
2023-07-01 07:52:19 -1000

    ;pkg:stack: bump stackage snapshots

--- Shake.hs
+++ Shake.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env stack
-{- stack script --resolver nightly-2023-06-21 --compile
+{- stack script --resolver nightly-2023-07-01 --compile
    --extra-include-dirs /Library/Developer/CommandLineTools/SDKs/MacOSX12.1.sdk/usr/include/ffi
    --package base-prelude
    --package directory
