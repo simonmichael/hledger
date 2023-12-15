e81430f05bc7b1f48348bfa06854f50d63550d35
Simon Michael <simon@joyful.com>
2023-12-15 13:00:42 -1000

    dev:web: rename makeFoundation* to makeApp*

--- hledger-web/Hledger/Web/Application.hs
+++ hledger-web/Hledger/Web/Application.hs
@@ -5,8 +5,8 @@
 
 module Hledger.Web.Application
   ( makeApplication
-  , makeFoundation
-  , makeFoundationWith
+  , makeApp
+  , makeAppWith
   ) where
 
 import Data.IORef (newIORef, writeIORef)
@@ -37,25 +37,29 @@ mkYesodDispatch "App" resourcesApp
 -- migrations handled by Yesod.
 makeApplication :: WebOpts -> Journal -> AppConfig DefaultEnv Extra -> IO Application
 makeApplication opts' j' conf' = do
-    foundation <- makeFoundation conf' opts'
-    writeIORef (appJournal foundation) j'
-    (logWare . (corsPolicy opts')) <$> toWaiApp foundation
+    app <- makeApp conf' opts'
+    writeIORef (appJournal app) j'
+    (logWare . (corsPolicy opts')) <$> toWaiApp app
   where
     logWare | development  = logStdoutDev
             | serve_ opts' || serve_api_ opts' = logStdout
             | otherwise    = id
 
-makeFoundation :: AppConfig DefaultEnv Extra -> WebOpts -> IO App
-makeFoundation conf opts' = do
-    manager <- newManager defaultManagerSettings
-    s <- staticSite
-    jref <- newIORef nulljournal
-    return $ App conf s manager opts' jref
+makeApp :: AppConfig DefaultEnv Extra -> WebOpts -> IO App
+makeApp = makeAppWith nulljournal
 
--- Make a Foundation with the given Journal as its state.
-makeFoundationWith :: Journal -> AppConfig DefaultEnv Extra -> WebOpts -> IO App
-makeFoundationWith j' conf opts' = do
-    manager <- newManager defaultManagerSettings
-    s <- staticSite
-    jref <- newIORef j'
-    return $ App conf s manager opts' jref
+-- Make an "App" (defined in App.hs), 
+-- with the given Journal as its state
+-- and the given "AppConfig" and "WebOpts" as its configuration.
+makeAppWith :: Journal -> AppConfig DefaultEnv Extra -> WebOpts -> IO App
+makeAppWith j' aconf wopts = do
+  s    <- staticSite
+  m    <- newManager defaultManagerSettings
+  jref <- newIORef j'
+  return App{
+      settings    = aconf
+    , getStatic   = s
+    , httpManager = m
+    , appOpts     = wopts
+    , appJournal  = jref
+    }
