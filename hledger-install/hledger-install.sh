65af4c344bb3a3788bac6ff2bf9c6cc6272353a3
Simon Michael <simon@joyful.com>
2024-01-28 01:02:06 -1000

    install: bump versions, build with lts-22/ghc 9.6.4

--- hledger-install/hledger-install.sh
+++ hledger-install/hledger-install.sh
@@ -13,7 +13,7 @@ set -o pipefail
 HLEDGER_INSTALL_TOOL=hledger-install.sh
 
 # This install script's version.
-HLEDGER_INSTALL_VERSION=20240104
+HLEDGER_INSTALL_VERSION=20240129
 
 # Tools to be installed by this install script, official tools first.
 # Keep synced with the package versions below.
@@ -41,12 +41,12 @@ pip \
 # Keep synced with the tools above. 
 # When changing remember to also bump HLEDGER_INSTALL_VERSION.
 # Official:
-HLEDGER_LIB_VERSION=1.32.2
-HLEDGER_VERSION=1.32.2
-HLEDGER_UI_VERSION=1.32.2
-HLEDGER_WEB_VERSION=1.32.2
+HLEDGER_LIB_VERSION=1.32.3
+HLEDGER_VERSION=1.32.3
+HLEDGER_UI_VERSION=1.32.3
+HLEDGER_WEB_VERSION=1.32.3
 # Third-party:
-HLEDGER_IADD_VERSION=1.3.19
+HLEDGER_IADD_VERSION=1.3.20
 HLEDGER_INTEREST_VERSION=1.6.6
 HLEDGER_EDIT_VERSION=1.14.0
 HLEDGER_PLOT_VERSION=1.14.0
@@ -95,7 +95,7 @@ STACK_MIN_VERSION=2.5.1
 # You can try specifying a different stackage version here, or 
 # commenting out this line to use your current global resolver,
 # to avoid unnecessary building.
-STACK_RESOLVER="--resolver=lts-22"
+STACK_RESOLVER="--resolver=lts-22.7"
 
 # Dependencies we require that aren't in the above stackage snapshot.
 # (Also requested when using cabal, but that's harmless.)
