6564db125c5eee01604acf6f9fc7c1e8709d24be
Simon Michael <simon@joyful.com>
2024-04-16 09:00:44 -1000

    ;install: 1.33

--- hledger-install/hledger-install.sh
+++ hledger-install/hledger-install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# Easy hledger installation script for POSIX systems, requiring bash
-# and some other POSIX tools.
-# This is based on get-stack.sh which is copyright (c) 2015-2017, Stack contributors.
+# Easy hledger installation script for POSIX systems, requiring bash and some other POSIX tools.
+# This is based on a snapshot of get-stack.sh which was copyright (c) 2015-2017, Stack contributors.
 
 # This was disabled as a workaround for https://github.com/simonmichael/hledger/issues/714
 # It has been left off so that one uninstallable tool doesn't block the others.
@@ -13,7 +12,7 @@ set -o pipefail
 HLEDGER_INSTALL_TOOL=hledger-install.sh
 
 # This install script's version.
-HLEDGER_INSTALL_VERSION=20240129
+HLEDGER_INSTALL_VERSION=20240417
 
 # Tools to be installed by this install script, official tools first.
 # Keep synced with the package versions below.
@@ -41,10 +40,10 @@ pip \
 # Keep synced with the tools above. 
 # When changing remember to also bump HLEDGER_INSTALL_VERSION.
 # Official:
-HLEDGER_LIB_VERSION=1.32.3
-HLEDGER_VERSION=1.32.3
-HLEDGER_UI_VERSION=1.32.3
-HLEDGER_WEB_VERSION=1.32.3
+HLEDGER_LIB_VERSION=1.33
+HLEDGER_VERSION=1.33
+HLEDGER_UI_VERSION=1.33
+HLEDGER_WEB_VERSION=1.33
 # Third-party:
 HLEDGER_IADD_VERSION=1.3.20
 HLEDGER_INTEREST_VERSION=1.6.6
@@ -95,12 +94,14 @@ STACK_MIN_VERSION=2.5.1
 # You can try specifying a different stackage version here, or 
 # commenting out this line to use your current global resolver,
 # to avoid unnecessary building.
-STACK_RESOLVER="--resolver=lts-22.7"
+STACK_RESOLVER="--resolver=lts-22.17"
 
 # Dependencies we require that aren't in the above stackage snapshot.
 # (Also requested when using cabal, but that's harmless.)
-# Quotes needed, be careful not to break interpolation in commands below, check with bash:
-STACK_EXTRA_DEPS=""
+# Be careful not to break interpolation in commands below, the format should be
+# STACK_EXTRA_DEPS="PKG1-VER1 PKG2-VER2 ..."
+# extra deps as in stack9.6.yaml:
+STACK_EXTRA_DEPS="process-1.6.19.0 Cabal-3.10.1.0 haskeline-0.8.2.1 vty-windows-0.2.0.1"
 
 #TODO? https://github.com/commercialhaskell/stack/issues/3055 https://github.com/haskell/hackage-security/issues/187
 #Updating package index Hackage (mirrored at https://s3.amazonaws.com/hackage.fpcomplete.com/) ...
