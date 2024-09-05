a1d83add56085aae64cf7b42e01280dde48b82c7
Simon Michael <simon@joyful.com>
2024-09-05 22:43:06 +0100

    ;dev: pkg: bump to latest nightly stackage snapshot

--- tools/docshelltest.hs
+++ tools/docshelltest.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env stack
-{- stack script --resolver nightly-2024-07-12 --compile
+{- stack script --resolver nightly-2024-09-04 --compile
 -}
 -- add this to see packages being installed instead of a long silence:
 --   --verbosity=info
