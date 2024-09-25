simonmichael/hledger

commit 2a99b3d45634f26cd62745ddce6136361001b3f8

imp: stack: build with ghc 9.8, latest stackage nightly

hledger-web/Hledger/Web/Handler/RegisterR.hs
@@ -11,6 +11,7 @@ module Hledger.Web.Handler.RegisterR where
 
 import Data.List (intersperse, nub, partition)
 import qualified Data.Text as T
+import Safe (tailDef)
 import Text.Hamlet (hamletFile)
 
 import Hledger
@@ -44,7 +45,7 @@ getRegisterR = do
       addCommas xs =
           zip xs $
           zip (map (T.unpack . accountSummarisedName . paccount) xs) $
-          tail $ (", "<$xs) ++ [""]
+          tailDef [""] $ (", "<$xs)
       items =
         styleAmounts (journalCommodityStylesWith HardRounding j) $
         accountTransactionsReport rspec{_rsQuery=q} j acctQuery

hledger-lib/package.yaml
@@ -39,7 +39,7 @@ extra-source-files:
 #data-files:
 
 dependencies:
-- base >=4.14 && <4.19
+- base >=4.14 && <4.20
 - base-compat
 - aeson >=1 && <2.3
 - aeson-pretty

hledger-ui/package.yaml
@@ -49,7 +49,7 @@ ghc-options:
 - -Wno-unused-do-bind
 
 dependencies:
-- base >=4.14 && <4.19
+- base >=4.14 && <4.20
 
  # curses is required to build terminfo for vty for hledger-ui.
  # On POSIX systems it might be not present.
@@ -98,7 +98,7 @@ library:
   - time >=1.5
   - transformers
   - vector
-  - brick >=2.1.1 && <2.3
+  - brick >=2.1.1 && <2.4
   - vty >=6.1 && <6.2
   - vty-crossplatform >= 0.4.0.0 && < 0.5.0.0
   when:
@@ -114,7 +114,7 @@ executables:
     source-dirs: app
     main: hledger-ui.hs
     dependencies:
-    - base >=4.14 && <4.19
+    - base >=4.14 && <4.20
     - hledger-ui
     when:
     - condition: flag(threaded)

hledger-web/package.yaml
@@ -78,7 +78,7 @@ when:
   ghc-options: -O0
 
 dependencies:
-- base >=4.14 && <4.19
+- base >=4.14 && <4.20
 
 library:
   source-dirs: .

hledger/Hledger/Cli/Script.hs
@@ -15,7 +15,7 @@ import Control.Concurrent as M
 import Control.Monad as M
 import Data.Char as M
 import Data.Either as M
-import Data.Functor as M
+import Data.Functor as M hiding (unzip)
 import Data.List as M
 import Data.Maybe as M
 import Data.Ord as M

hledger/package.yaml
@@ -99,7 +99,7 @@ ghc-options:
 - -optP-Wno-nonportable-include-path
 
 dependencies:
-- base >=4.14 && <4.19
+- base >=4.14 && <4.20
 - hledger-lib >=1.32.99 && <1.33
 - aeson >=1 && <2.3
 - ansi-terminal >=0.9

stack9.8.yaml
@@ -1,20 +1,37 @@
-# stack build plan using GHC 9.8.1 (WIP)
+# stack build plan using GHC 9.8.1
 
-resolver: ghc-9.8.1
+resolver: nightly-2024-01-04
 
 packages:
 - hledger-lib
-# - hledger
-# - hledger-ui
-# - hledger-web
+- hledger
+# blocked by https://github.com/luite/hfsevents/issues/16
+#- hledger-ui
+- hledger-web
 
-# extra-deps:
+extra-deps:
+- cassava-megaparsec-2.1.0
+- email-validate-2.3.2.19
+- yesod-1.6.2.1
+- yesod-form-1.7.6
+
+# work around https://github.com/stackbuilders/cassava-megaparsec/issues/39
+allow-newer: true
+
+# not working for me
+# allow-newer-deps:
+# - cassava-megaparsec
+# - vector
+
 
 nix:
   pure: false
   packages: [perl gmp ncurses zlib]
 
-# ghc-options:
+ghc-options:
+  # silence 9.8's new partial warnings for now
+  "$locals": -Wno-x-partial
+
 #   "$locals": -fplugin Debug.Breakpoint
 
 # # for precise profiling, per https://www.tweag.io/posts/2020-01-30-haskell-profiling.html:

commit 69f20178b01b1a4c5cd0a62341a5a1ab82cb3ba9

web: RegisterR.getRegisterR.addCommas: do not drop last account
Bug was introduced in commit 2a99b3d45634f26cd62745ddce6136361001b3f8
in an effort to get rid of partial List.tail,
because GHC-9.8 started to warn about it.

Problem is that the rewritten code with tailDef always removes the last account,
whereas the original intention was to replace all accounts by a comma except the last one.
addCommas should prepare a comma separated list like List.intercalate.

hledger-web/Hledger/Web/Handler/RegisterR.hs
@@ -9,9 +9,9 @@
 
 module Hledger.Web.Handler.RegisterR where
 
+import qualified Data.List.NonEmpty as NonEmpty
 import Data.List (intersperse, nub, partition)
 import qualified Data.Text as T
-import Safe (tailDef)
 import Text.Hamlet (hamletFile)
 
 import Hledger
@@ -42,10 +42,11 @@ getRegisterR = do
           map (\(acct,(name,comma)) -> (acct, (T.pack name, T.pack comma))) .
           undecorateLinks . elideRightDecorated 40 . decorateLinks .
           addCommas . preferReal . otherTransactionAccounts q acctQuery
+      snoc xs x = NonEmpty.prependList xs $ NonEmpty.singleton x
       addCommas xs =
           zip xs $
           zip (map (T.unpack . accountSummarisedName . paccount) xs) $
-          tailDef [""] $ (", "<$xs)
+          NonEmpty.tail $ snoc (", "<$xs) ""
       items =
         styleAmounts (journalCommodityStylesWith HardRounding j) $
         accountTransactionsReport rspec{_rsQuery=q} j acctQuery