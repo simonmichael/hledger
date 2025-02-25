e160a1c496658216bc45832a23281b557f445e2c
Thomas Miedema <thomasmiedema@gmail.com>
2025-02-25 13:14:50 +0100

    ;cln: unittest.hs: remove bothersome PackageImports
    When running `cd hledger-lib && ghci test/unittest.hs`, ghci complains
    with:
    
    ```
      test/unittest.hs:7:1: error:
          Could not find module ‘Hledger’
          It is not a module in the current program, or in any known package.
        |
      7 | import "hledger-lib" Hledger (tests_Hledger)
        | ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      Failed, no modules loaded.
    ```
    
    This commit removes the "hledger-lib" package-qualified import, such
    that above ghci command works as expected.
    
    However, there is a comment in hledger-lib/test/unittest.hs that says:
    
    > package-qualified import to avoid cabal missing-home-modules warning
    > (and double-building ?)
    
    The missing-home-modules warning and the double building can indeed be
    reproduced by running (after removing the "hledger-lib"
    package-qualified import): `cd hledger-lib && cabal build unittest`. It
    will first build `hledger-lib`, then show a warning about
    missing-home-modules, and then build `hledger-lib` again.
    
    After comparing the unittest sections of hledger.cabal and
    hledger-lib.cabal, the solution turned out to be to remove `./` from
    hs-source-dirs for unittest. Don't ask me why though!
    
    Overall it's a nice cleanup.

--- hledger-lib/test/unittest.hs
+++ hledger-lib/test/unittest.hs
@@ -2,9 +2,7 @@
 Run the hledger-lib package's unit tests using the tasty test runner.
 -}
 
--- package-qualified import to avoid cabal missing-home-modules warning (and double-building ?)
-{-# LANGUAGE PackageImports #-}
-import "hledger-lib" Hledger (tests_Hledger)
+import Hledger (tests_Hledger)
 import System.Environment (setEnv)
 import Test.Tasty (defaultMain)
 
--- hledger/test/unittest.hs
+++ hledger/test/unittest.hs
@@ -3,9 +3,6 @@ Run the hledger package's unit tests using the tasty test runner
 (by running the test command limited to Hledger.Cli tests).
 -}
 
--- cabal missing-home-modules workaround from hledger-lib, seems not needed here
--- {-# LANGUAGE PackageImports #-}
--- import "hledger" Hledger.Cli (tests_Hledger_Cli)
 import Hledger.Cli (tests_Hledger_Cli)
 import System.Environment (setEnv)
 import Test.Tasty (defaultMain)
