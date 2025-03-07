498e45e5d32786147ce52a278fd7cec0f7f75440
Simon Michael <simon@joyful.com>
2025-03-06 22:06:33 -1000

    ;pkg:hledger-install: bump to 1.42

--- hledger-install/hledger-install.sh
+++ hledger-install/hledger-install.sh
@@ -3,19 +3,19 @@
 # This was based on get-stack.sh which was copyright (c) 2015-2017, Stack contributors.
 
 # This install script's version.
-HLEDGER_INSTALL_VERSION=20241212
+HLEDGER_INSTALL_VERSION=20250307
 
 # Package versions to be installed by this install script.
 # Keep synced with the tools above.
 # When changing remember to also bump HLEDGER_INSTALL_VERSION.
 # Official:
-HLEDGER_LIB_VERSION=1.41
-HLEDGER_VERSION=1.41
-HLEDGER_UI_VERSION=1.41
-HLEDGER_WEB_VERSION=1.41
+HLEDGER_LIB_VERSION=1.42
+HLEDGER_VERSION=1.42
+HLEDGER_UI_VERSION=1.42
+HLEDGER_WEB_VERSION=1.42
 # Third-party:
 HLEDGER_IADD_VERSION=1.3.21
-HLEDGER_INTEREST_VERSION=1.6.6
+HLEDGER_INTEREST_VERSION=1.6.7
 HLEDGER_EDIT_VERSION=1.14.0
 HLEDGER_PLOT_VERSION=1.14.0
 HLEDGER_LOTS_VERSION=0.4.2
@@ -25,12 +25,12 @@ PRICEHIST_VERSION=1.4.12
 # You can try specifying a different stackage version here, or
 # commenting out this line to use your current global resolver,
 # to avoid unnecessary building.
-STACKAGE_SNAPSHOT=lts-23
+STACKAGE_SNAPSHOT=nightly-2025-03-01
 
-# If nny required haskell dependencies aren't in the above stackage snapshot,
+# If any required haskell dependencies aren't in the above stackage snapshot,
 # list them here in this format: "PKG1-VER1 PKG2-VER2.."
 # (one line, don't break interpolation in commands below).
-STACK_EXTRA_DEPS="base-compat-0.14.0"
+STACK_EXTRA_DEPS=""
 
 # Tools to be installed by this install script, official tools first.
 # Keep synced with the package versions below.
