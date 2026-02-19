a05b2364bba5e70b5e388979b5f513c70971c293
Simon Michael <simon@joyful.com>
2026-02-18 22:59:50 -1000

    imp:lots: show source position in lots: tag validation errors
    Add csourcepos field to Commodity type and capture it during parsing,
    so lots: tag validation errors show file:line and a megaparsec-style
    excerpt like other hledger parse errors.

--- hledger-lib/Hledger/Read/JournalReader.hs
+++ hledger-lib/Hledger/Read/JournalReader.hs
@@ -621,15 +621,16 @@ commoditydirectivep = commoditydirectiveonelinep <|> commoditydirectivemultiline
 -- >>> Right _ <- rjp commoditydirectiveonelinep "commodity $1.00 ; blah\n"
 commoditydirectiveonelinep :: JournalParser m ()
 commoditydirectiveonelinep = do
-  (off, Amount{acommodity,astyle}) <- try $ do
+  (off, pos, Amount{acommodity,astyle}) <- try $ do
     string "commodity"
+    pos <- getSourcePos
     lift skipNonNewlineSpaces1
     off <- getOffset
     amt <- amountp
-    pure $ (off, amt)
+    pure $ (off, pos, amt)
   lift skipNonNewlineSpaces
   (comment, tags) <- lift transactioncommentp
-  let comm = Commodity{csymbol=acommodity, cformat=Just $ dbg7 "style from commodity directive" astyle, ccomment=comment, ctags=tags}
+  let comm = Commodity{csymbol=acommodity, cformat=Just $ dbg7 "style from commodity directive" astyle, ccomment=comment, ctags=tags, csourcepos=pos}
   if isNothing $ asdecimalmark astyle
   then customFailure $ parseErrorAt off pleaseincludedecimalpoint
   else modify' (\j -> j{jdeclaredcommodities=M.insert acommodity comm $ jdeclaredcommodities j
@@ -653,13 +654,14 @@ pleaseincludedecimalpoint = chomp $ unlines [
 commoditydirectivemultilinep :: JournalParser m ()
 commoditydirectivemultilinep = do
   string "commodity"
+  pos <- getSourcePos
   lift skipNonNewlineSpaces1
   sym <- lift commoditysymbolp
   (comment, tags) <- lift transactioncommentp
   -- read all subdirectives, saving format subdirectives as Lefts
   subdirectives <- many $ indented (eitherP (formatdirectivep sym) (lift restofline))
   let mfmt = lastMay $ lefts subdirectives
-  let comm = Commodity{csymbol=sym, cformat=mfmt, ccomment=comment, ctags=tags}
+  let comm = Commodity{csymbol=sym, cformat=mfmt, ccomment=comment, ctags=tags, csourcepos=pos}
   modify' (\j -> j{jdeclaredcommodities=M.insert sym comm $ jdeclaredcommodities j
                   ,jdeclaredcommoditytags=if null tags then jdeclaredcommoditytags j
                                           else M.insert sym tags $ jdeclaredcommoditytags j})
