9a05929355ff31603e36d1ee35d7b53a6e1f1beb
Simon Michael <simon@joyful.com>
2026-06-16 20:46:45 +0100

    ;bin: hledger-script-example-short.hs: cleanups

--- bin/hledger-script-example-short.hs
+++ bin/hledger-script-example-short.hs
@@ -1,10 +1,9 @@
 #!/usr/bin/env stack
---  stack runghc --package hledger
-{-
+-- stack runghc --package hledger
+-- or, for more setup/build progress output:
+-- stack runghc --package hledger --verbosity info
 
-script-example - my new script
-
--}
+-- hledger-script-example-short - a short hledger stack script example
 
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -14,20 +13,16 @@ import Data.Text.IO qualified as T
 
 cmdmode = hledgerCommandMode (unlines
     ---------------------------standard terminal width-----------------------------
-  ["script-example"
-  ,"Usage: hledger-script-example [OPTS] [ARGS]"
-  ,"or:    hledger script-example -- [OPTS] [ARGS]"
+  ["script-example-short"
+  ,"Usage: hledger-script-example-short [OPTS] [ARGS]"
+  ,"or:    hledger script-example-short -- [OPTS] [ARGS]"
   ,"Examples:"
-  ,"$ hledger-script-example         # do the thing"
-  ,"$ hledger-script-example --help  # print help"
+  ,"$ hledger-script-example-short         # do the thing"
+  ,"$ hledger-script-example-short --help  # print help"
   ])
   [] [generalflagsgroup1] [] ([], Just $ argsFlag "[ARGS]")
 
 main = do
   opts@CliOpts{reportspec_=rspec} <- getHledgerCliOpts cmdmode
   withJournal opts $ \j -> do
-    putStrLn "hello"
-
-
-
-
+    putStrLn $ "successfully read journal: " <> journalFilePath j
